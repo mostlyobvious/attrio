7741ebff2842a5086e3a6fe6cb32c4059dbcbb4f
Paweł Pacana <pawel.pacana@gmail.com>
2024-01-16 23:01:37 +0100

    Re-init rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,8 @@
-require 'rubygems'
-
-$:.unshift File.expand_path('..', __FILE__)
-$:.unshift File.expand_path('../../lib', __FILE__)
-
-require 'attrio'
 require 'rspec'
 require 'webmock/rspec'
 require 'json'
+
+require_relative "../lib/attrio"
 
 Class.class_eval do
   def const_missing(name)
@@ -15,15 +11,14 @@ Class.class_eval do
 end
 
 RSpec.configure do |config|
-  config.include WebMock::API
-  config.order = :rand
-  config.color_enabled = true
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-
-  config.before(:each) do
-    WebMock.reset!
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
-  config.after(:each) do
-    WebMock.reset!
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
   end
+  config.disable_monkey_patching!
+  config.warnings = true
+  config.order = :random
+  Kernel.srand config.seed
 end
