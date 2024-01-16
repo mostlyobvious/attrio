af9f91f5a87fda83d0a3dd76a166b37ee64be0e3
Paweł Pacana <pawel.pacana@gmail.com>
2024-01-16 23:33:56 +0100

    Too much to expect at the moment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,8 +17,8 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
-  config.disable_monkey_patching!
-  config.warnings = true
+  # config.disable_monkey_patching!
+  # config.warnings = true
   config.order = :random
   Kernel.srand config.seed
 end
